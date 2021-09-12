85e04928b8997e518751218e2201a4942fd92d4f
Fercho Ulloa <ferulloac6@gmail.com>
2021-09-12 18:02:21 -0500

    Calculadora

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,30 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'practica2';
+  title = 'Primer programa';
+  numero1: number;
+  numero2: number;
+  resultado: number;
+
+  public operar(op: string): void{
+    switch(op){
+      case 's':
+        this.resultado = this.numero1 + this.numero2;
+        break;
+      case 'r':
+        this.resultado = this.numero1 - this.numero2;
+        break;
+      case 'm':
+        this.resultado = this.numero1 * this.numero2;
+        break;
+      case 'd':
+        this.resultado = this.numero1 / this.numero2;
+        break;
+      default:
+        this.resultado = 0;
+    }
+
+  }
+
+
 }
